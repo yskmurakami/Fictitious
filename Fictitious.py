5086c3925e982a7f75bc0714103f716084822a6c
Yusuke Murakami <carrot.yummy@gmail.com>
2014-06-19 00:05:33 +0900

    ver1.1.0 Numpyを導入して少し前身。大概の操作が２行ずつ進んでいるので、まずはそこを１行で進められるようにしていく。

--- Fictitious.py
+++ Fictitious.py
@@ -1,33 +1,35 @@
 #まずはprocedualに
 import matplotlib.pyplot as plt
+import numpy as np
 from random import uniform
 fig, ax = plt.subplots()
 
 n = 1000 #the count of repeat
+s = ((1,-1),(-1,1),
+     (-1,1),(1,-1)) #利得行列
+
 x0 = [uniform(0, 1)]
 x1 = [uniform(0, 1)]
-s = ((1,-1),(-1,1),(-1,1),(1,-1)) #利得行列
+
+def ss(a, s):
+    return ((s[0][a], s[1][a]),
+            (s[2][a], s[3][a]))
+
+s0 = np.array(ss(0, s))
+s1 = np.array(ss(1, s))
 
 for i in range(n-1):
-    if (s[0][0]*x0[i]+s[1][0]*(1-x0[i]) > s[2][0]*x0[i]+s[3][0]*(1-x0[i])):
-        a0 = 0
-    elif (s[0][0]*x0[i]+s[1][0]*(1-x0[i]) < s[2][0]*x0[i]+s[3][0]*(1-x0[i])):
-        a0 = 1
-    else:
-        a0 = 0 #本当はランダムで0か1
-    
-    if (s[0][1]*x1[i]+s[2][1]*(1-x1[i]) > s[1][1]*x1[i]+s[3][1]*(1-x1[i])):
-        a1 = 0
-    elif (s[0][1]*x1[i]+s[2][1]*(1-x1[i]) < s[1][1]*x1[i]+s[3][1]*(1-x1[i])):
-        a1 = 1
-    else:
-        a1 = 0 #本当はランダムで0か1
+    vec0 = (1-x0[i], x0[i])
+    vec1 = (1-x1[i], x1[i])
+
+    A0 = np.dot(s0, vec0)
+    A1 = np.dot(s1, vec1)
+    a0 = A0.argmax() #同じだったときのrandomが表現できず少し不正確
+    a1 = A1.argmax()
 
     x0.append(x0[i]+(a1-x0[i])/(i+2))
     x1.append(x1[i]+(a0-x1[i])/(i+2))
 ax.plot(x0, 'r-')
 ax.plot(x1, 'b-')
 plt.show()
-
 #if文の中身をObjectにして裏で動かすとObject指向らしくなる
-
